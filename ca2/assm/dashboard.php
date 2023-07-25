4b0eb0d2f85bdd97d9704130ff771a48ac009b96
tomnyson <tabletkindfire@gmail.com>
2023-07-25 11:31:17 +0700

    layout categories

--- ca2/assm/dashboard.php
+++ ca2/assm/dashboard.php
@@ -1,58 +1,16 @@
 <!doctype html>
 <html lang="en">
 <?php
-session_start();
-require('./user.php');
-if (!isset($_SESSION['currentUser']) || $_SESSION['currentUser']->role != '1') {
-    var_dump($_SESSION['currentUser']);
-    header('Location: login.php');
-}
+include('./layout/header.php')
 ?>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/dashboard.css">
-</head>
 
 <body>
     <div class="container">
         <div class="row">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Dashboard</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <div class="flex">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Logout</a>
-                                </li>
-                            </div>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <div class="col col-lg-2">
-                <ul class="list-group">
-                    <li class="list-group-item">Users</li>
-                    <li class="list-group-item">
-                        <a href="./categories.php">categories</a>
-                    </li>
-                    <li class="list-group-item">Product</li>
-                    <li class="list-group-item">Order</li>
-                    <li class="list-group-item">Report</li>
-                </ul>
-            </div>
+            <?php
+            include('./layout/nav.php');
+            include('./layout/sidebar.php');
+            ?>
             <div class="col col-lg-10">
                 <table class="table">
                     <thead>
@@ -86,8 +44,9 @@ if (!isset($_SESSION['currentUser']) || $_SESSION['currentUser']->role != '1') {
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script> -->
 </body>
 
 </html>
--- ca2/assm/provider.php
+++ ca2/assm/provider.php
@@ -7,8 +7,6 @@ $connection = NULL;
 try {
     $url = "mysql:host=" . HOST . ";dbname=" . DB_NAME . "";
     $connection = new PDO($url, USERNAME, PASSWORD);
-    var_dump($url);
-    echo "success";
 } catch (ErrorException $error) {
     echo $error;
 }
